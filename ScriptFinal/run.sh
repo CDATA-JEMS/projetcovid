14caa9b360844054f2909c26fc5892ee11eb8d00
OlvCh <35966684+OlvCh@users.noreply.github.com>
2021-02-24 23:11:51 +0100

    Update run.sh
    corrections mineures

--- ScriptFinal/run.sh
+++ ScriptFinal/run.sh
@@ -1,27 +1,23 @@
-#Preparer par équipe : NIKNAM Pedram,BEN AMMAR Amira, CHICOUARD Olivier, SUN Andelai. 
-#Dans le cadre de projet:Impact de la Covid sur les entreprises en Europe
+#Par l'équipe : NIKNAM Pedram,BEN AMMAR Amira, CHICOUARD Olivier, SUN Andelai. 
+#Dans le cadre du projet : Impact de la Covid sur les entreprises en Europe
 #
-#Script principal qui permet de lance automatiquement plusieur d'autre script 
-#les services prérequis pour executions:
-#Python3, Git, Wget, HDAS, PIG, HIVE, IMPALA, 
+#Script principal qui permet de lancer automatiquement les autres scripts 
+#les services prérequis pour execution:
+#Python3, Git, Wget, HDFS, PIG, HIVE, IMPALA, 
 #
-#Script rendre execcutables les autres scriptes 
+#Script pour rendre execcutables les autres scripts 
 #
-#première partie
-#Il lance le script chargmtCovid.sh pour télécharge les données sur covide19 à partir de git
-#Il lance le script repCovidPig.sh pour preparer le repertoire et envoies des données en HDFS 
+#premiere partie
+#Il lance le script chargmtCovid.sh pour télécharger les données sur covid19 à partir de git
+#Il lance le script repCovidPig.sh pour preparer le repertoire et envoyer les données sur HDFS 
 #Il lance le covid.pig  pour nettoyer et normaliser les données.
 #
 #deuxieme partie 
-#Il lance le script chargmtLiqEnt.sh pour télécharge les données sur liquidation entreprise à partir de insee
-#Il lance le script repLiqEnt.sh pour preparer le repertoire et envoies des données en HDFS 
+#Il lance le script chargmtLiqEnt.sh pour télécharger les données sur liquidation entreprise à partir de insee
+#Il lance le script repLiqEnt.sh pour preparer le repertoire et envoyer des données sur HDFS 
 #
 #Troisième partie
 #Il lance le script pyhon 
-
-
-
-
 
 
 
@@ -29,44 +25,43 @@
 chmod +x *.sh
 
 
-
 ####################################
-# lancement de script pour chargement des données COVID-19 dans mondes
+# lancement du script de chargement des données COVID-19 mondiales
 ./chargmtCovid.sh
 sleep 3m
 
-# lancement de script pour preparation de repertoire de covide en HDFS
+# lancement du script de preparation des repertoires covid19 sur HDFS
 ./repCovidPig.sh
 sleep 3m
 
-# lancement de  Pig shell de Covid, pour netoyager, structurer et normaliser les données
+# lancement de  Pig shell de Covid, pour nettoyer, structurer et normaliser les données
 pig covid.pig
 sleep 3m
 
 ####################################
-# lancement de script pour chargement des données sur liqudation des entreprise en france
+# lancement du script de chargement des données de liqudation des entreprises en France
 ./chargmtLiqEnt.sh
 sleep 3m
 
-# lancement de script pour preparation de repertoire de Liquidation d'entreprise en HDFS
+# lancement du script de preparation des repertoires de liquidation des entreprises sur HDFS
 ./repLiqEnt.sh
 sleep 3m
 
-# lancement de  Pig shell de LiqEnt, pour netoyager, structurer et normaliser les données
+# lancement du shell Pig de LiqEnt, pour nettoyer, structurer et normaliser les données
 pig LiqEnt.pig
 sleep 3m
 
 
 ####################################
-# lancement de script pour chargement des données sur chomage en Europe
+# lancement du script de chargement des données du chomage en Europe
 pyhton chomage.py
 sleep 1m
 
-# lancement de script pour preparation de repertoire de Liquidation d'entreprise en HDFS
+# lancement du script de preparation des repertoire de chomage sur HDFS
 ./repChomage.sh
 sleep 3m
 
-# lancement de  Pig shell de LiqEnt, pour netoyager, structurer et normaliser les données
+# lancement du shell Pig shell chomage, pour nettoyer, structurer et normaliser les données
 pig chomage.pig
 sleep 3m
 
