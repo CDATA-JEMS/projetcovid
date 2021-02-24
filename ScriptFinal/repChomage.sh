852eba430754284d225f2a68f227da17bd4b3251
OlvCh <35966684+OlvCh@users.noreply.github.com>
2021-02-25 00:07:13 +0100

    corrections mineures

--- ScriptFinal/repChomage.sh
+++ ScriptFinal/repChomage.sh
@@ -1,11 +1,11 @@
-#Preparer par équipe : NIKNAM Pedram,BEN AMMAR Amira, CHICOUARD Olivier, SUN Andelai. 
-#Dans le cadre de projet:Impact de la Covid sur les entreprises en Europe
-#Script est lancé autmatiquement par run.sh
-#Scripte doit être lance après le script chomage.pig
-#Script pour preparation de repertoire de Pig pour les données sur chomage en europe
-#Envoie des données au HDFS, 
-#suppression des anciens données s'il existe
-#Cloudera doit être lancer avant d'execution de cette script
+#Par l'equipe : NIKNAM Pedram,BEN AMMAR Amira, CHICOUARD Olivier, SUN Andelai. 
+#Dans le cadre du projet : Impact de la Covid sur les entreprises en Europe
+#Script lancé automatiquement par run.sh
+#Script à lancer après le script chomage.pig
+#Script de preparation de repertoire de Pig pour les données sur chomage en europe
+#Envoi des données au HDFS, 
+#suppression des anciennes données s'il elles existent
+#Cloudera doit être lance avant d'execution de ce script
 
 hadoop fs -mkdir -p /user/cloudera/datalake/prepared_data/chomage
 
