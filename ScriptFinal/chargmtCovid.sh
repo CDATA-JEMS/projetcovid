d1486a2731958ac5d380d3b86036cebc999cd282
OlvCh <35966684+OlvCh@users.noreply.github.com>
2021-02-24 22:26:05 +0100

    Update chargmtCovid.sh

--- ScriptFinal/chargmtCovid.sh
+++ ScriptFinal/chargmtCovid.sh
@@ -1,15 +1,15 @@
-#Preparer par équipe : NIKNAM Pedram,BEN AMMAR Amira, CHICOUARD Olivier, SUN Andelai. 
-#Dans le cadre de projet:Impact de la Covid sur les entreprises en Europe
-#Script est lancé autmatiquement par run.sh
-#Scripte doit être lance avant le script repCovidpig.sh
-# script prepare repertoir puis telecharge et separ les données covid19 dans les diff repertoir
+#Par l'équipe : NIKNAM Pedram,BEN AMMAR Amira, CHICOUARD Olivier, SUN Andelai. 
+#Dans le cadre du projet : Impact de la Covid sur les entreprises en Europe
+#Script lancé autmatiquement par run.sh
+#Ce script doit être lancé avant le script repCovidpig.sh
+#Ce script prépare les fichiers du répertoire puis télécharge et sépare les données covid19 dans les différents repertoires
 
-#supression d'ancien données dans le cas d'existance
+#supression d'anciennes données en cas d'existence
 rm -rf COVID-19
 
 git clone https://github.com/CSSEGISandData/COVID-19.git
 
-# suppression des coma entre les doubles cote
+# suppression des coma entre les doubles quote
 sed -i ':a;s/^\(\([^"]*,\?\|"[^",]*",\?\)*"[^",]*\),/\1 /;ta' COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/*.csv
 
 # Replace original file dates to match the dates included in the file names
@@ -19,8 +19,8 @@ for f in COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/*.csv; do
     touch -d "$(date -d $fdate)" $f
 done
 
-# creation de repertoir et copie des fichier dans les diff repertoir par rapport a leur type de date
-mkdir format1
+# creation de repertoires et copie des fichiers dans les diff repertoires par rapport a leur type de date
+mkdir format1 
 mkdir format2
 
 find COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/ -maxdepth 1 -not -newermt "2020-03-21" -exec basename \{} .po \; | grep csv | sort | xargs -I % mv COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/% format1/
